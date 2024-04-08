1cefc9a94ca1b03be7e0ba7cc5aea048457cdd5d
GiltMonster <lucassantoslukas2@gmail.com>
2024-04-08 00:34:11 -0300

    feat: CRUD administrador feito, falta estilo !!

---
+++ php/admin.php
@@ -0,0 +1,34 @@
+<?php
+session_start(); // inicia a sessão
+require_once('conexao.php'); // inclui os métodos de conexão do arquivo conexao.php
+
+if (!isset($_SESSION['admin_logado'])) { // se não existir a sessão admin_logado
+    header('Location: login.php'); // redireciona para a página login.php
+    exit(); // finaliza a execução do script
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') { // se o formulário foi submetido
+    $adm_nome = $_POST['adm_nome']; // recebe o nome adm
+    $adm_email = $_POST['adm_email']; // recebe email
+    $adm_senha = $_POST['adm_senha']; // recebe senha
+    $adm_ativo = isset($_POST['adm_ativo'])?1:0; //recebe informação se esta ativo
+    
+
+    try {
+        $sql = "INSERT INTO ADMINISTRADOR (ADM_NOME, ADM_EMAIL, ADM_SENHA, ADM_ATIVO) 
+        VALUES (:adm_nome, :adm_email, :adm_senha, :adm_ativo)"; // consulta SQL
+        $query = $pdo->prepare($sql); // prepara a consulta SQL
+        $query->bindParam(':adm_nome', $adm_nome, PDO::PARAM_STR);
+        $query->bindParam(':adm_email', $adm_email, PDO::PARAM_STR);
+        $query->bindParam(':adm_senha', $adm_senha, PDO::PARAM_STR);
+        $query->bindParam(':adm_ativo', $adm_ativo, PDO::PARAM_STR);
+
+        $query->execute(); // executa a consulta SQL
+
+        echo "<p style='color:green;'>Produto cadastrado com sucesso!</p>"; // mensagem de sucesso
+
+    } catch (PDOException $e) {
+        echo "<p style='color:red;'>Erro ao cadastrar o administrador: " . $e->getMessage() . "</p>"; // mensagem de erro
+    }
+}
+?>
---
+++ php/listar_administrador.php
@@ -0,0 +1,64 @@
+<?php
+    session_start(); //inicia a sessão
+
+    require_once('./conexao.php'); //inclui o arquivo de conexão
+
+    if (!isset($_SESSION['admin_logado'])) { //se não existir a sessão admin_logado
+        header('Location:login.php'); //redireciona para a página login.php
+        exit(); //finaliza a execução do script
+    }
+
+    try{
+        $stmt = $pdo->prepare("SELECT * FROM ADMINISTRADOR");
+        $stmt->execute();
+        if ($stmt->rowCount() > 0) { //se retornar algum registro
+            $adms = $stmt->fetchAll(PDO::FETCH_ASSOC); //retorna um array associativo com os registros
+        } else {
+            $adms = [];
+        }
+    } catch (PDOException $e) {
+        echo "<p style='color:red;'>Erro ao listar os administradores: " . $e . "</p>"; //mensagem de erro
+    }
+
+?>
+
+
+<!DOCTYPE html>
+    <html lang="pt-br">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Listar Administradores</title>
+    </head>
+
+    <body>
+        <h2>Lista de Administradores</h2>
+        <table border="1">
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>Senha</th>
+                <th>Ativo</th>
+                <th>Excluir</th>
+                <th>Editar</th>
+            </tr>
+            <?php
+            foreach ($adms as $adm) {
+                echo "<tr>";
+                echo "<td>" . $adm['ADM_ID'] . "</td>";
+                echo "<td>" . $adm['ADM_NOME'] . "</td>";
+                echo "<td>" . $adm['ADM_EMAIL'] . "</td>";
+                echo "<td>" . $adm['ADM_SENHA'] . "</td>";
+                echo "<td>" . $adm['ADM_ATIVO'] . "</td>";
+                echo "<td><a href='./excluir_administrador.php?id=" . $adm['ADM_ID'] . "'>Excluir</a></td>";
+                echo "<td><a href='./editar_admin.php?adm_id=" . $adm['ADM_ID'] . "'>Editar</a></td>";
+                echo "</tr>";
+            }
+            ?>
+        </table>
+        <p><a href="./cadastrar_administrador.php">Cadastrar Administrador</a></p>
+    </body>
+
+    </html>
