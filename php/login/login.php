c46f907e5ce67f0b6adc74c540abc7243a8f8acb
GiltMonster <lucassantoslukas2@gmail.com>
2024-04-15 15:22:54 -0300

    feat: login funcionando

---
+++ php/login/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+    <h2>Login do Administrador</h2>
+
+    <form action="processa_login.php" method="post">
+        <label for="adm_email">E-mail:</label>
+        <input type="email" id="adm_email" name="adm_email" required>
+        <p></p>
+        <label for="adm_senha">Senha</label>
+        <input type="password" id="adm_senha" name="adm_senha" required>
+        <p></p>
+
+        <input type="submit" value="Entrar">
+
+        <?php
+        if (isset($_GET['erro'])) { //se existir o parâmetro erro na URL
+            echo '<p style="color:red;">Nome de usuário ou senha incorretos!</p>';
+        }
+        ?>
+    </form>
+</body>
+
+</html>
